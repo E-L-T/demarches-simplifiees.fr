7ef0364af09bd7250b7c1067c7d8a26f6d75e69a
Paul Chavard <pro@paul.chavard.net>
2022-11-22 16:18:10 +0100

    fix(:champ_routage): improuve update_dossier_groupe_instructeur_id

--- app/models/champs/routage_champ.rb
+++ app/models/champs/routage_champ.rb
@@ -1,2 +1,14 @@
 class Champs::RoutageChamp < Champs::TextChamp
+  before_create :set_groupe_instructeur_id
+  after_update :update_dossier_groupe_instructeur_id
+
+  private
+
+  def set_groupe_instructeur_id
+    self.value = dossier.groupe_instructeur_id
+  end
+
+  def update_dossier_groupe_instructeur_id
+    dossier.update_column(:groupe_instructeur_id, value)
+  end
 end
